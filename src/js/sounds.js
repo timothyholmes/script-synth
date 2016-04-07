3f675130ce354dc8835fd7ec42007998bea2dd28
Tim Holmes <tholmes@starchase.com>
2016-04-07 13:37:06 -0400

    added sounds and triggers

--- src/js/sounds.js
+++ src/js/sounds.js
@@ -1,11 +1,11 @@
-function F(context, position) {
+function Key(context, frequency) {
 	var _this = this;
 
 	_this.context = context;
-	_this.position = position;
+	_this.frequency = frequency;
 };
 
-F.prototype.setup = function() {
+Key.prototype.setup = function() {
 	var _this = this;
 
 	_this.osc = _this.context.createOscillator();
@@ -14,18 +14,17 @@ F.prototype.setup = function() {
 	_this.gain.connect(_this.context.destination)
 };
 
-F.prototype.trigger = function(time) {
+Key.prototype.trigger = function(time) {
 	var _this = this;
 
 	_this.setup();
 
-	_this.osc.frequency.setValueAtTime(150, time);
-	_this.gain.gain.setValueAtTime(1.5, time);
+	_this.osc.frequency.value = _this.frequency;
+	_this.gain.gain.value = 1.5;
 
-	_this.osc.frequency.exponentialRampToValueAtTime(0.01, time + 1);
-	_this.gain.gain.exponentialRampToValueAtTime(0.01, time + 1);
+	_this.osc.frequency.exponentialRampToValueAtTime(0.01, time + 0.3);
+	_this.gain.gain.exponentialRampToValueAtTime(0.01, time + 0.3);
 
 	_this.osc.start(time);
-
-	_this.osc.stop(time + 1);
+	_this.osc.stop(time + 0.5);
 };
--- src/js/synth.js
+++ src/js/synth.js
@@ -3,7 +3,39 @@
 	angular.module('thScriptSynth')
 
 	.controller('SynthController', ['$scope', function($scope){
+		var _this = this;
+		var context = new (window.AudioContext || window.webkitAudioContext)();
 
+		var keyBoard = {
+			lowf: new Key(context, 87.31),
+			lowfs: new Key(context, 92.50),
+			lowg: new Key(context, 98.00),
+			lowgs: new Key(context, 103.83),
+			lowa: new Key(context, 110.00),
+			lowas: new Key(context, 116.54),
+			lowb: new Key(context, 123.47),
+			lowc: new Key(context, 130.81),
+			lowcs: new Key(context, 138.59),
+			lowd: new Key(context, 146.83),
+			lowds: new Key(context, 155.56),
+			lowe: new Key(context, 164.81),			
+			highf: new Key(context, 174.61),
+			highfs: new Key(context, 185.00),
+			highg: new Key(context, 196.00),
+			highgs: new Key(context, 207.65),
+			higha: new Key(context, 220.00),
+			highas: new Key(context, 233.08),
+			highb: new Key(context, 246.94),
+			highc: new Key(context, 261.63),
+			highcs: new Key(context, 277.18),
+			highd: new Key(context, 293.66),
+			highds: new Key(context, 311.13),
+			highe: new Key(context, 329.63)
+		}
+		
+		_this.playSound = function(type) {
+			keyBoard[type].trigger(context.currentTime);
+		}
 	}])
 
 	.directive('thSynth', function () {
